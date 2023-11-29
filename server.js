d2d44f546acca1cf2ef4c02f56a8d05e7c44abdb
Saish Kothawade <saish.kothawade20@pccoepune.org>
2023-11-29 09:09:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-/*  @file       server.js
-*   @authors    Saurabh Gupta     [saurabh.gupta1002@gmail.com]
-*               Awadhut Thube     [awadhutthube@gmail.com]
-*               Jheel Nagaria     [nagariajheel@gmail.com]
-*               Ashish Kamble     [ashishkamble14@gmail.com]
-*/
-
 // Instances //
 var app = require('express')();
 var http = require('http').Server(app);
